7b2d894ee6310ba45bd78d3d2d505222d660d050
Arik Kfir <arik@kfirs.com>
2021-10-07 00:51:38 +0300

    Reformat.

--- terraform/300-github.tf
+++ terraform/300-github.tf
@@ -5,58 +5,30 @@ data "github_user" "arikkfir" {
 }
 
 module "github-config-repository" {
-  source = "./modules/github-repository"
-  name = ".github"
+  source      = "./modules/github-repository"
+  name        = ".github"
   description = "GitHub configuration repository."
-  topics = [
-    "github",
-    "infrastructure",
-    "iac",
-  ]
+  topics      = ["github", "infrastructure", "iac"]
 }
 
 module "github-infrastructure-repository" {
-  source = "./modules/github-repository"
-  name = "infrastructure"
+  source      = "./modules/github-repository"
+  name        = "infrastructure"
   description = "Infrastructure-as-Code for my infrastructure."
-  topics = [
-    "fluxcd",
-    "infrastructure",
-    "iac",
-    "kubernetes",
-    "kustomize",
-    "terraform",
-  ]
+  topics      = ["fluxcd", "infrastructure", "iac", "kubernetes", "kustomize", "terraform"]
 }
 
 module "github-cloudflare-operator-repository" {
-  source = "./modules/github-repository"
-  name = "cloudflare-operator"
+  source      = "./modules/github-repository"
+  name        = "cloudflare-operator"
   description = "Kubernetes operator for Cloudflare resources."
-  topics = [
-    "go",
-    "dns",
-    "kubernetes",
-    "devops",
-    "cloudflare",
-    "operator",
-    "k8s",
-    "cloudflare-operator",
-  ]
+  topics      = ["go", "dns", "kubernetes", "devops", "cloudflare", "operator", "k8s", "cloudflare-operator"]
 }
 
 module "github-syncer-repository" {
-  source = "./modules/github-repository"
-  name = "syncer"
-  description = "Synchronizes properties between Kubernetes resources"
-  requires_status_checks = [
-    "build"
-  ]
-  topics = [
-    "go",
-    "kubernetes",
-    "devops",
-    "operator",
-    "k8s",
-  ]
+  source                 = "./modules/github-repository"
+  name                   = "syncer"
+  description            = "Synchronizes properties between Kubernetes resources"
+  requires_status_checks = ["build"]
+  topics                 = ["go", "kubernetes", "devops", "operator", "k8s"]
 }
