2d08812b1e85cae59432536bc24f5c377620d6a6
Arik Kfir <arik@kfirs.com>
2021-11-27 12:03:16 +0200

    Set initial node count for node pools

--- terraform/200-gke.tf
+++ terraform/200-gke.tf
@@ -146,10 +146,20 @@ resource "google_container_node_pool" "core-n2-custom-2-4096-pre" {
   cluster = google_container_cluster.production.name
   name = "core-n2-custom-2-4096-pre"
   location = var.gcp_zone
+
+  # Scaling
+  initial_node_count = 1
   autoscaling {
     min_node_count = 0
     max_node_count = 3
   }
+  lifecycle {
+    ignore_changes = [
+      initial_node_count # see https://github.com/hashicorp/terraform-provider-google/issues/6901#issuecomment-667369691
+    ]
+  }
+
+  # Operations
   management {
     auto_repair = true
     auto_upgrade = true
@@ -180,10 +190,20 @@ resource "google_container_node_pool" "work-n2-custom-4-4096-pre" {
   cluster = google_container_cluster.production.name
   name = "work-n2-custom-4-4096-pre"
   location = var.gcp_zone
+
+  # Scaling
+  initial_node_count = 0
   autoscaling {
     min_node_count = 0
     max_node_count = 3
   }
+  lifecycle {
+    ignore_changes = [
+      initial_node_count # see https://github.com/hashicorp/terraform-provider-google/issues/6901#issuecomment-667369691
+    ]
+  }
+
+  # Operations
   management {
     auto_repair = true
     auto_upgrade = true
