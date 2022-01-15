a32c9ce6482293e18d0173c82c80a49dfb259993
Arik Kfir <arik@kfirs.com>
2022-01-15 15:48:48 +0200

    Grant devbot admin to Helm repository bucket

--- terraform/gcp/400-helm-repository.tf
+++ terraform/gcp/400-helm-repository.tf
@@ -6,14 +6,20 @@ resource "google_storage_bucket" "arikkfir-helm-repository" {
   uniform_bucket_level_access = true
 }
 
-resource "google_storage_bucket_iam_member" "public-access" {
+resource "google_storage_bucket_iam_member" "helm-repository-public-access" {
   bucket = google_storage_bucket.arikkfir-helm-repository.name
   role = "roles/storage.objectViewer"
   member = "allUsers"
 }
 
-resource "google_storage_bucket_iam_member" "helm-infra-admin" {
+resource "google_storage_bucket_iam_member" "helm-repository-infrastructure-admin" {
   bucket = google_storage_bucket.arikkfir-helm-repository.name
   role = "roles/storage.objectAdmin"
   member = "serviceAccount:${google_service_account.infrastructure-github-actions.email}"
+}
+
+resource "google_storage_bucket_iam_member" "helm-repository-devbot-admin" {
+  bucket = google_storage_bucket.arikkfir-helm-repository.name
+  role = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.devbot-github-actions.email}"
 }
