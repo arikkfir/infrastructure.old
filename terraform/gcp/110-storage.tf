fc979d44f53758021fee7149c86da3f93bcce616
Arik Kfir <arik@kfirs.com>
2022-04-01 22:27:23 +0300

    Refactor Deployment infrastructure
    This change overhauls the deployment infrastructure, aiming to install
    a process of deployment, utilizing GitHub features such as
    Pull Requests, Actions, Deployments, etc.

--- terraform/gcp/110-storage.tf
+++
@@ -1,7 +0,0 @@
-resource "google_storage_bucket" "arikkfir-devops" {
-  name = "arikkfir-devops"
-  location = "EU"
-  project = google_project.project.project_id
-  storage_class = "MULTI_REGIONAL"
-  uniform_bucket_level_access = true
-}
