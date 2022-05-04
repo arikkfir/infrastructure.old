aec08b9c9e46e6bfaaa19023733ff28a6addb944
Arik Kfir <arik@kfirs.com>
2022-05-04 17:29:59 +0300

    Extract config-connector IAM to separate file
    Split config-connector IAM service account from 200-gke-iam.tf into
    200-gke-config-connector.tf for better separation.

--- cloud-provider/200-gke-iam.tf
+++ cloud-provider/200-gke-iam.tf
@@ -20,28 +20,3 @@ resource "google_project_iam_member" "gke-node" {
   role    = each.key
   member  = "serviceAccount:${google_service_account.gke-node.email}"
 }
-
-resource "google_service_account" "config-connector" {
-  project      = data.google_project.project.project_id
-  account_id   = "config-connector"
-  display_name = "GKE Config Connector"
-}
-
-# TODO: infer the "cnrm-system/cnrm-controller-manager" value
-resource "google_service_account_iam_member" "config-connector_workload_identity" {
-  service_account_id = google_service_account.config-connector.name
-  role               = "roles/iam.workloadIdentityUser"
-  member             = "serviceAccount:${data.google_project.project.project_id}.svc.id.goog[cnrm-system/cnrm-controller-manager]"
-}
-
-resource "google_project_iam_member" "config-connector" {
-  for_each = toset([
-    "roles/iam.serviceAccountAdmin",
-    "roles/logging.logWriter",
-    "roles/monitoring.metricWriter",
-  ])
-
-  project = data.google_project.project.project_id
-  role    = each.key
-  member  = "serviceAccount:${google_service_account.config-connector.email}"
-}
