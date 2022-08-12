004712bc6667db152f26d043be939865f7f06063
Arik Kfir <arik@kfirs.com>
2022-08-12 16:03:07 +0300

    Improve Terraform import scripts

--- import_google_project_service_apis.sh
+++ import_google_project_service_apis.sh
@@ -2,24 +2,11 @@
 
 set -exu
 
-terraform import 'google_project_service.apis["artifactregistry.googleapis.com"]' arikkfir/artifactregistry.googleapis.com
-terraform import 'google_project_service.apis["bigquery.googleapis.com"]' arikkfir/bigquery.googleapis.com
-terraform import 'google_project_service.apis["bigquerystorage.googleapis.com"]' arikkfir/bigquerystorage.googleapis.com
-terraform import 'google_project_service.apis["cloudbilling.googleapis.com"]' arikkfir/cloudbilling.googleapis.com
-terraform import 'google_project_service.apis["cloudresourcemanager.googleapis.com"]' arikkfir/cloudresourcemanager.googleapis.com
-terraform import 'google_project_service.apis["compute.googleapis.com"]' arikkfir/compute.googleapis.com
-terraform import 'google_project_service.apis["container.googleapis.com"]' arikkfir/container.googleapis.com
-terraform import 'google_project_service.apis["containerfilesystem.googleapis.com"]' arikkfir/containerfilesystem.googleapis.com
-terraform import 'google_project_service.apis["containerregistry.googleapis.com"]' arikkfir/containerregistry.googleapis.com
-terraform import 'google_project_service.apis["dns.googleapis.com"]' arikkfir/dns.googleapis.com
-terraform import 'google_project_service.apis["iam.googleapis.com"]' arikkfir/iam.googleapis.com
-terraform import 'google_project_service.apis["iamcredentials.googleapis.com"]' arikkfir/iamcredentials.googleapis.com
-terraform import 'google_project_service.apis["logging.googleapis.com"]' arikkfir/logging.googleapis.com
-terraform import 'google_project_service.apis["monitoring.googleapis.com"]' arikkfir/monitoring.googleapis.com
-terraform import 'google_project_service.apis["oslogin.googleapis.com"]' arikkfir/oslogin.googleapis.com
-terraform import 'google_project_service.apis["pubsub.googleapis.com"]' arikkfir/pubsub.googleapis.com
-terraform import 'google_project_service.apis["secretmanager.googleapis.com"]' arikkfir/secretmanager.googleapis.com
-terraform import 'google_project_service.apis["servicemanagement.googleapis.com"]' arikkfir/servicemanagement.googleapis.com
-terraform import 'google_project_service.apis["serviceusage.googleapis.com"]' arikkfir/serviceusage.googleapis.com
-terraform import 'google_project_service.apis["storage-api.googleapis.com"]' arikkfir/storage-api.googleapis.com
-terraform import 'google_project_service.apis["storage-component.googleapis.com"]' arikkfir/storage-component.googleapis.com
+declare -a apis=("artifactregistry" "bigquery" "bigquerystorage" "cloudbilling" "cloudresourcemanager" "compute"
+                  "container" "containerregistry" "containerfilesystem" "dns" "iam" "iamcredentials" "logging"
+                  "monitoring" "oslogin" "pubsub" "secretmanager" "servicemanagement" "serviceusage" "storage-api"
+                  "storage-component")
+
+for api in "${apis[@]}"; do
+  terraform import "google_project_service.apis[\"${api}.googleapis.com\"]" "arikkfir/${api}.googleapis.com"
+done
