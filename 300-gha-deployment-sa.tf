6e68e99bc736be4bfe474b5f9786a550f31f4be6
Arik Kfir <arik@kfirs.com>
2022-08-13 10:03:00 +0300

    Add cluster deployment service accounts
    This change adds the "cluster" and "deployment" GCP service accounts.

--- 300-gha-deployment-sa.tf
+++ 300-gha-deployment-sa.tf
@@ -1,3 +1,9 @@
+resource "google_service_account" "gha-arikkfir-cluster" {
+  project      = data.google_project.project.project_id
+  account_id   = "gha-arikkfir-cluster"
+  display_name = "GitHub Actions: arikkfir/cluster"
+}
+
 resource "google_service_account" "gha-arikkfir-deployment" {
   project      = data.google_project.project.project_id
   account_id   = "gha-arikkfir-deployment"
