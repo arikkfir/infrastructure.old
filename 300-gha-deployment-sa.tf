9adb39c2597b047281fa21b5c22ea01a458b8164
Arik Kfir <arik@kfirs.com>
2022-08-13 10:17:45 +0300

    Add GKE permissions to Cluster/Deployment SAs
    This change adds the "Kubernetes Engine Developer" role to the cluster
    and deployment service accounts.

--- 300-gha-deployment-sa.tf
+++ 300-gha-deployment-sa.tf
@@ -4,8 +4,28 @@ resource "google_service_account" "gha-arikkfir-cluster" {
   display_name = "GitHub Actions: arikkfir/cluster"
 }
 
+resource "google_project_iam_member" "gha-arikkfir-cluster" {
+  for_each = toset([
+    "roles/container.developer",
+  ])
+
+  project = data.google_project.project.project_id
+  role    = each.key
+  member  = "serviceAccount:${google_service_account.gha-arikkfir-cluster.email}"
+}
+
 resource "google_service_account" "gha-arikkfir-deployment" {
   project      = data.google_project.project.project_id
   account_id   = "gha-arikkfir-deployment"
   display_name = "GitHub Actions: arikkfir/deployment"
+}
+
+resource "google_project_iam_member" "gha-arikkfir-deployment" {
+  for_each = toset([
+    "roles/container.developer",
+  ])
+
+  project = data.google_project.project.project_id
+  role    = each.key
+  member  = "serviceAccount:${google_service_account.gha-arikkfir-deployment.email}"
 }
