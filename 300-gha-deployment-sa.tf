c34c4569265b5b45195cf4483bd26a15de95b9b5
Arik Kfir <arik@kfirs.com>
2022-08-14 23:18:30 +0300

    Fix Kubernetes permissions to "cluster" and "deployment" SAs (#28)
    This change grants the "Kubernetes Engine Admin" role to the "cluster" &
    "deployment" service accounts.

--- 300-gha-deployment-sa.tf
+++ 300-gha-deployment-sa.tf
@@ -1,19 +1,3 @@
-resource "google_service_account" "gha-arikkfir-cluster" {
-  project      = data.google_project.project.project_id
-  account_id   = "gha-arikkfir-cluster"
-  display_name = "GitHub Actions: arikkfir/cluster"
-}
-
-resource "google_project_iam_member" "gha-arikkfir-cluster" {
-  for_each = toset([
-    "roles/container.developer",
-  ])
-
-  project = data.google_project.project.project_id
-  role    = each.key
-  member  = "serviceAccount:${google_service_account.gha-arikkfir-cluster.email}"
-}
-
 resource "google_service_account" "gha-arikkfir-deployment" {
   project      = data.google_project.project.project_id
   account_id   = "gha-arikkfir-deployment"
@@ -22,7 +6,7 @@ resource "google_service_account" "gha-arikkfir-deployment" {
 
 resource "google_project_iam_member" "gha-arikkfir-deployment" {
   for_each = toset([
-    "roles/container.developer",
+    "roles/container.admin",
   ])
 
   project = data.google_project.project.project_id
