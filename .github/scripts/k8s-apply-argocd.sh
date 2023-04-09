dd10563cb16d55960cb27937896b3486743b3603
arikkfir <arik@kfirs.com>
2023-04-09 23:30:13 +0300

    Fix ArgoCD deployment

--- .github/scripts/k8s-apply-argocd.sh
+++ .github/scripts/k8s-apply-argocd.sh
@@ -4,10 +4,10 @@ set -exuo pipefail
 
 kubectl apply -k ./argocd/server
 kubectl config set-context --current --namespace=argocd
-kubectl wait --for=jsonpath='{.status.availableReplicas}'=1 --timeout=0 statefulset -l app.kubernetes.io/part-of=argocd
-kubectl wait --for=jsonpath='{.status.readyReplicas}'=1 --timeout=0 statefulset -l app.kubernetes.io/part-of=argocd
-kubectl wait --for=condition=Available=True --timeout=300s deployment -l app.kubernetes.io/part-of=argocd
-kubectl wait --for=condition=Available=True --timeout=300s deployment -l app.kubernetes.io/part-of=argocd-applicationset
+kubectl wait --for=jsonpath='{.status.availableReplicas}'=1 --timeout=30m statefulset -l app.kubernetes.io/part-of=argocd
+kubectl wait --for=jsonpath='{.status.readyReplicas}'=1 --timeout=30m statefulset -l app.kubernetes.io/part-of=argocd
+kubectl wait --for=condition=Available=True --timeout=30m deployment -l app.kubernetes.io/part-of=argocd
+kubectl wait --for=condition=Available=True --timeout=30m deployment -l app.kubernetes.io/part-of=argocd-applicationset
 
 if [[ $(kubectl get secret | grep -c "argocd-initial-admin-secret") == "1" ]]; then
   # TODO: another way to do this is to add the new ArgoCD password to the file (in a 2nd line after the initial one) so command reads 2 lines from file
