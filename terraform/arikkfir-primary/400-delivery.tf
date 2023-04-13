3e60fb906f775b38229872aa5f203c12323f684b
arikkfir <arik@kfirs.com>
2023-04-13 16:24:36 +0300

    Deploy delivery pipeline IAM service account

---
+++ terraform/arikkfir-primary/400-delivery.tf
@@ -0,0 +1,17 @@
+resource "google_service_account" "delivery" {
+  project      = data.google_project.arikkfir-primary.project_id
+  account_id   = "delivery"
+  display_name = "Delivery pipeline"
+}
+
+# Set of permissions that the config-connector SA needs
+resource "google_project_iam_member" "arikkfir-primary-delivery" {
+  for_each = toset([
+    "roles/container.clusterViewer",
+    "roles/container.viewer",
+  ])
+
+  project = data.google_project.arikkfir-primary.project_id
+  role    = each.key
+  member  = "serviceAccount:${google_service_account.delivery.email}"
+}
