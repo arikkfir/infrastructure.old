d1ac5c9a849a0ce10b56ae7bfdb21c2f599c62b3
arikkfir <arik@kfirs.com>
2023-04-13 17:14:29 +0300

    Allow delivery SA to authenticate from GHA

--- terraform/arikkfir-primary/400-delivery.tf
+++ terraform/arikkfir-primary/400-delivery.tf
@@ -15,3 +15,10 @@ resource "google_project_iam_member" "arikkfir-primary-delivery" {
   role    = each.key
   member  = "serviceAccount:${google_service_account.gha-arikkfir-delivery.email}"
 }
+
+# Allow delivery pipeline service account to authenticate from GitHub Actions
+resource "google_service_account_iam_member" "gha-arikkfir-primary-delivery-workload-identity-user" {
+  service_account_id = google_service_account.gha-arikkfir-delivery.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${data.google_iam_workload_identity_pool.github-actions.name}/attribute.repository/arikkfir/delivery"
+}
